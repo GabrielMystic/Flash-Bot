7f2103ce924f1965599535b487cc72f33e047cb0
Gabriel <122692044+GabrielMystic@users.noreply.github.com>
2023-01-14 14:48:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Lightning\nBot', {
+say('Flash\nBot', {
 font: 'chrome',
 align: 'center',
 gradient: ['blue', 'white']})
