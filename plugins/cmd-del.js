d8b692f5e50dd5039160b796c01734ddf45c4950
Gabriel <122692044+GabrielMystic@users.noreply.github.com>
2023-02-24 17:35:46 -0300

    Update cmd-del.js

--- plugins/cmd-del.js
+++ plugins/cmd-del.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix, text, command }) => {
 let hash = text
 if (m.quoted && m.quoted.fileSha256) hash = m.quoted.fileSha256.toString('hex')
-if (!hash) throw `*[❗INFO❗] SOLO SE PUEDE ASIGNAR TEXTO O COMANDO A STICKER  E IMAGBE, PARA OBTENER EL CODIGO ASIGNADO USE EL COMANDO ${usedPrefix}listcmd*`
+if (!hash) throw `*[❗INFO❗] SOLO SE PUEDE ASIGNAR TEXTO O COMANDO A STICKER  E IMAGEN, PARA OBTENER EL CODIGO ASIGNADO USE EL COMANDO ${usedPrefix}listcmd*`
 let sticker = global.db.data.sticker
 if (sticker[hash] && sticker[hash].locked) throw '*[❗INFO❗] SOLO EL OWNER PUEDE REALIZAR ESTA MODIFICACIÓN*'
 delete sticker[hash]
